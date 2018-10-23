feb6f6cfde0861b6dc2e23cdd53c0dfb2788af15
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-10-23 11:31:30 +0300

    Add routes for list of all tasks and separate tasks.

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,15 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/tasks', function () {
+    $tasks = DB::table('tasks')->get();
+    return view('tasks.index', compact('tasks'));
+});
+
+Route::get('/tasks/{task}', function ($id) {
+    $tasks = DB::table('tasks')->find($id);
+
+    return view('tasks.show', compact('tasks'));
 });
 
 Route::get('/about', function () {
