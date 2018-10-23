c7bfbf89da880f32e2e1598d066e7cff4301043d
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-10-23 12:06:32 +0300

    Add task model.

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,16 @@
 |
 */
 
+use App\Task;
+
 Route::get('/tasks', function () {
-    $tasks = DB::table('tasks')->get();
+    $tasks = Task::all();
+
     return view('tasks.index', compact('tasks'));
 });
 
 Route::get('/tasks/{task}', function ($id) {
-    $tasks = DB::table('tasks')->find($id);
+    $tasks = Task::find($id);
 
     return view('tasks.show', compact('tasks'));
 });
