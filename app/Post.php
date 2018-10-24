2dc214d2134277e8e0cae6054fb6f3a696e9a3e2
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-10-24 16:22:09 +0300

    Add archives and filter method.

--- app/Post.php
+++ app/Post.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use Carbon\Carbon;
+
 class Post extends Model
 {
     public function comments(){
@@ -14,5 +16,24 @@ class Post extends Model
 
     public function addComment($body){
         $this->comments()->create(compact('body'));
+    }
+
+    public function scopeFilter($query, $filters){
+        if($month = $filters['month'] ?? false){
+            $query->whereMonth('created_at', Carbon::parse($month)->month);
+        }
+
+        if($year = $filters['year'] ?? false){
+            $query->whereYear('created_at', $year);
+        }
+
+    }
+
+    public static function archives(){
+        return static::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
+            ->groupBy('year', 'month')
+            ->orderByRaw('min(created_at) desc')
+            ->get()
+            ->toArray();
     }
 }
