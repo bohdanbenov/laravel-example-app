0ecbd49834b2d1ac1da943f6d862f251af6c4d69
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-10-24 11:32:29 +0300

    Add block with comments on view.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,6 +10,26 @@
 
         <hr>
 
+        <div class="card">
+            <div class="card-block">
+                <form method="post" action="/posts/{{ $post->id }}/comments">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <textarea name="body" placeholder="Your comment here." class="form-control"></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Add Comment</button>
+                    </div>
+                </form>
+
+                @include('layouts.errors')
+
+            </div>
+        </div>
+
+        <hr>
+
         <div class="comments">
             <ul class="list-group">
                 @foreach($post->comments as $comment)
