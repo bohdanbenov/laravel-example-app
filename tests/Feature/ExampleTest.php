4fba6e2bcaaeba12cc9e40d0c47bc431078fc681
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-10-24 17:24:16 +0300

    Change test for `assertSee()` functionality.

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -18,4 +18,9 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function testMainPageSee()
+    {
+        $this->get('/')->assertSee('The Bootstrap Blog');
+    }
 }
