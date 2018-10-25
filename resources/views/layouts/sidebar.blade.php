751497a55be093f40d6ceb21f4816aca64347429
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-10-25 11:55:00 +0300

    Display tags in sidebar.

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -13,6 +13,18 @@
             @endforeach
         </ol>
     </div>
+
+    <div class="sidebar-module">
+        <h4>Tags</h4>
+        <ol class="list-unstyled">
+            @foreach($tags as $tag)
+                <li>
+                    <a href="/posts/tags/{{ $tag }}">{{ $tag }}</a>
+                </li>
+            @endforeach
+        </ol>
+    </div>
+
     <div class="sidebar-module">
         <h4>Elsewhere</h4>
         <ol class="list-unstyled">
@@ -21,4 +33,4 @@
             <li><a href="#">Facebook</a></li>
         </ol>
     </div>
-</div><!-- /.blog-sidebar -->
+</div>
